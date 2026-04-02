3a8118b8b272903198334cb8101f9167d96293e3
dtbackjisu209 <duongtungbach20092005@gmail.com>
2026-04-02 22:28:39 +0700

    fix(auth): improve auth middleware and update config

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import express from 'express';
 import cors from 'cors'; 
 import { errorHandler } from './core/handler/error-handle.js';
 import postRouter from './modules/post/post.route.js';
+import authRouter from './modules/auth/auth.route.js';
 
 const app = express();
 
 app.use(cors()); 
 app.use(express.json());
 app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/auth', authRouter);
 app.use(errorHandler);
 
 export default app;
